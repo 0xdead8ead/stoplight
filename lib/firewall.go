81b15440751383e53ca4e498f3970cf9a79c9caa
Ch6t9v0 <chase_schultz@gap.com>
2014-06-10 17:57:09 -0700

    Added Firewall Data Structures

--- lib/firewall.go
+++ lib/firewall.go
@@ -3,9 +3,34 @@ package firewall
 import (
 	//"fmt"
 	"labix.org/v2/mgo"
+	"log"
 )
 
+type firewall_request struct {
+	requestor         string
+	service_date      string
+	service_term_date string
+	leader            string
+	business_approver string
+	service_number    string
+	firewall_rules    []firewall_rule
+}
+
+type firewall_rule struct {
+	source_zone      string
+	source_ips       string
+	dest_zone        string
+	data_type        string
+	network_protocol string
+	dest_ips         string
+	dest_ports       string
+	app_name         string
+	server_loc       string
+}
+
+//Saves Firewall Request to MongoDB
 func SaveFirewall(fwreq map[string]interface{}) {
+	log.Println("%s", fwreq)
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
